7517deed09bbfb89a14163bf0ec550ab8ef963ee
root <root@tethys-staging.byu.edu>
2022-08-18 21:25:27 +0000

    Warehouse_Commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_namespace_packages
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_resource_files
 
 # -- Apps Definition -- #
 app_package = 'hydro_var_monitor'
@@ -15,6 +15,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/public', 't
 resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
 
 
+resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
 setup(
     name=release_package,
     version='0.0.4',
